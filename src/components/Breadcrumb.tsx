1a873e19887dba1dcb097e968582c924624e382b
Saul Florez <sflorez@globaltask.net>
2023-03-08 16:55:00 -0400

    📦 NEW: Componente breadcrumb

---
+++ src/components/Breadcrumb.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+type BreadcrumbItem = {
+  name: string;
+  link: string;
+  isActive?: boolean;
+};
+
+type BreadcrumbProps = { items: Array<BreadcrumbItem> };
+
+export const Breadcrumb = ({ items }: BreadcrumbProps): JSX.Element => (
+  <ul className="breadcrumb">{items.map(BreadcrumbItem)}</ul>
+);
+
+export const BreadcrumbItem = ({
+  name,
+  link,
+  isActive,
+}: BreadcrumbItem): JSX.Element => {
+  return (
+    <li
+      className={`breadcrumb__item ${
+        isActive ? "breadcrumb__item--active" : ""
+      }`}
+    >
+      <Link to={link}>{name}</Link>
+    </li>
+  );
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Breadcrumb } from "../components/Breadcrumb";
 
 export const HomePage = (): JSX.Element => {
   return (
     <div className="home">
-      <h1 className="home__title">Productos destacados</h1>
+      <div className="home__header">
+        <h1 className="home__title">Productos destacados</h1>
+        <Breadcrumb
+          items={[
+            {
+              name: "Todos los productos",
+              link: "/products",
+              isActive: true,
+            },
+            { name: "Cocina", link: "/kitchen" },
+            { name: "Productos Destacados", link: "/" },
+          ]}
+        />
+      </div>
     </div>
   );
 };
