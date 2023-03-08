ec306a74a8c3b3a14230cd7573593a1098a7f267
Saul Florez <sflorez@globaltask.net>
2023-03-08 13:16:35 -0400

    🎉 INIT: Iniciando el proyecto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/naming-convention */
+import React from "react";
+import { Header } from "./components/Header";
+import "./styles/main.scss";
+
+/**
+ * @returns component
+ */
+const App = (): JSX.Element => {
+  return <Header />;
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = (): JSX.Element => {
+  return (
+    <header>
+      <h1 className="text-primary text-center">Patterson Agency Challenge</h1>
+    </header>
+  );
+};
