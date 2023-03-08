c33b621740a3dd6f7f45fd4b1cce548a5cb144d7
Saul Florez <sflorez@globaltask.net>
2023-03-08 14:04:18 -0400

    📦 NEW: Componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { CartIcon } from "../assets/icons/cartIcon";
 
 export const Header = (): JSX.Element => {
   return (
-    <header>
-      <h1 className="text-primary text-center">Patterson Agency Challenge</h1>
+    <header className="header">
+      <div className="header__content">
+        <img
+          src="../assets/images/patterson-agency-logo.png"
+          className="header__logo"
+          alt="Patterson Agency Logo"
+        />
+        <div className="header__cart">
+          <div className="header__cart__icon">
+            <CartIcon />
+            <div className="header__cart__badge">0</div>
+          </div>
+          <div className="header__cart__info">
+            <span className="header__cart__amount">0,00 €</span>
+            <span>Mi carrito</span>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
