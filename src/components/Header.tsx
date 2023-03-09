0ff613a1b240361e8e27ebb6776d8720dc7094af
Saul Florez <sflorez@globaltask.net>
2023-03-09 13:25:24 -0400

    📦 NEW: Agregando enlaces

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { CartIcon } from "../assets/icons/cartIcon";
 
 export const Header = (): JSX.Element => {
   return (
     <header className="header">
       <div className="header__content">
-        <img
-          src="../assets/images/patterson-agency-logo.png"
-          className="header__logo"
-          alt="Patterson Agency Logo"
-        />
+        <Link to="/">
+          <img
+            src="../assets/images/patterson-agency-logo.png"
+            className="header__logo"
+            alt="Patterson Agency Logo"
+          />
+        </Link>
         <div className="header__cart">
           <div className="header__cart__icon">
             <CartIcon />
             <div className="header__cart__badge">0</div>
           </div>
-          <div className="header__cart__info">
+          <Link to="/cart" className="header__cart__info">
             <span className="header__cart__amount">0,00 €</span>
             <span>Mi carrito</span>
-          </div>
+          </Link>
         </div>
       </div>
     </header>
